a512969185f76493bb32cf620fd68aae49fbe244
Jose Alem Rodriguez Valverde <josealem03@gmail.com>
2024-08-01 16:55:32 -0400

    Pasos para que puedan realizar la compra y obtener la entrada en el home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import Step1 from '../assets/images/1.svg';
+import Step2 from '../assets/images/2.svg';
+import Step3 from '../assets/images/3.svg';
 
 function Home() {
     return (
-        <div className="container mx-auto px-4 py-16">
-        <h1 className="text-4xl font-bold text-gray-800 text-center">Bienvenido al Evento de Blockchain</h1>
-        <p className="text-gray-600 text-center mt-4">Regístrate y compra tus entradas para el evento.</p>
+        <div className="container mx-auto px-4 py-12">
+            <h1 className="text-5xl font-bold text-gray-800 text-center mb-8">Bienvenido al Evento de Blockchain</h1>
+            <p className="text-xl text-gray-600 text-center mb-12">Sigue los pasos para la compra de entradas del evento.</p>
+            
+            <div className="flex flex-wrap justify-center gap-16">
+                <StepCard 
+                    image={Step1}
+                    step="Paso 1"
+                    description="Coloca tus datos personales"
+                />
+                <StepCard 
+                    image={Step2}
+                    step="Paso 2"
+                    description="Paga la entrada mediante QR"
+                />
+                <StepCard 
+                    image={Step3}
+                    step="Paso 3"
+                    description="Obtienes tu entrada!!"
+                />
+            </div>
+        </div>
+    );
+}
+
+function StepCard({ image, step, description }) {
+    return (
+        <div className="flex flex-col items-center w-80">
+            <img src={image} alt={step} className="w-full h-72 object-contain mb-6"/>
+            <h3 className="text-2xl font-semibold text-gray-800 mb-2">{step}</h3>
+            <p className="text-center text-xl text-gray-600">{description}</p>
         </div>
     );
 }
